d6d9c3bd25aea883a3662ac677379e614535fd85
hajime matsumoto <hajime.matsumoto@avap.co.jp>
2015-07-23 03:15:17 +0900

    update

--- builder/workspace/files/init/mysql.sh
+++ builder/workspace/files/init/mysql.sh
@@ -7,6 +7,9 @@ DATA_DIR=/var/lib/mysql
 LOG_DIR=/var/log/mysql
 [ -d $LOG_DIR ] || mkdir -p $LOG_DIR
 
+[ -s /etc/mysql/my.cnf ] || cp /origin/etc/mysql/my.cnf /etc/mysql/my.cnf
+
+
 [ -d ${DATA_DIR}/mysql ] || {
     pushd /usr
     mysql_install_db --user=mysql --ldata=$DATA_DIR
